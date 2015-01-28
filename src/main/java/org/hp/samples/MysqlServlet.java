24c2a4b3df365bfccf540da59e08c847e724f0f7
Arun Jacob <arun.x.arun@gmail.com>
2015-01-28 14:01:43 -0800

    fixed packaging bug
    package was misnamed. fixed to match folder structure. org.java shouldn't have been in the name.

--- src/main/java/org/hp/samples/MysqlServlet.java
+++ src/main/java/org/hp/samples/MysqlServlet.java
@@ -19,7 +19,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 ============================================================================ */
 
-package main.java.org.hp.samples;
+package org.hp.samples;
 
 import java.io.IOException;
 import java.io.PrintWriter;
